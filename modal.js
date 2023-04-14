353e6b687ccadd1e23dc1f84e9d16464d78517ad
hunterAlbertJ <albert.hunter13@gmail.com>
2023-04-13 21:21:14 -0700

    blog April11 added, fixed an edge case where opening blog through url would open two blogs if the modal id existed within another modal id

--- modal.js
+++ modal.js
@@ -2,8 +2,19 @@ $(document).ready(function () {
     $('.modal').each(function () {
         const modalId = `#${$(this).attr('id')}`;
         console.log(modalId)
-        if (window.location.href.indexOf(modalId) !== -1) {
+        if (window.location.href.indexOf(modalId) !== -1 && indexCheck(modalId)) {
             $(modalId).modal('show');
         }
     });
 });
+
+function indexCheck(modalId){
+    let idLength = modalId.length
+    let indexPosition = window.location.href.indexOf(modalId)
+    let urlLength = window.location.href.length
+    if(idLength + indexPosition == urlLength){
+        return true;
+    } else {
+        return false;
+    }
+}
